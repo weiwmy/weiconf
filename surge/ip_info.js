51f35a4ccabfa8f6eb2d2f290b7b75e7e79bcf9b
weiwmy <weiwmy@gmail.com>
2023-09-29 22:24:23 +0800

    Update lists.

--- surge/ip_info.js
+++ surge/ip_info.js
@@ -1,24 +1,40 @@
-const url = "http://ip-api.com/json";
+// Define the URL for the IP information API
+let url = "http://ip-api.com/json"
 
-$httpClient.get(url, (error, response, data) => {
-  if (!error) {
-    const { country, city, isp, query, countryCode } = JSON.parse(data);
-    const emoji = getFlagEmoji(countryCode);
-    const content = `IP: ${query}\nISP: ${isp}\nLocation: ${emoji}${country} - ${city}`;
-
-    $done({
-      title: "IP Check",
-      content,
-      icon: "globe.asia.australia.fill"
-    });
-  } else {
-    $done({ error });
-  }
+// Make an HTTP GET request to the API and handle the response
+$httpClient.get(url, function(error, response, data){
+    // Parse the JSON data from the response
+    let jsonData = JSON.parse(data)
+    
+    // Extract information from the JSON response
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  
+    // Create a structured body object with information
+    body = {
+        title: "IP Check",
+        content: `IP: ${ip}\nISP: ${isp}\nLocation: ${emoji}${country} - ${city}`,
+        icon: "globe.asia.australia.fill"
+    }
+  
+    // Finish the script and pass the body object as the result
+    $done(body);
 });
 
+// Function to get a flag emoji based on a country code
 function getFlagEmoji(countryCode) {
-  if (countryCode === 'TW') {
-    countryCode = 'CN';
-  }
-  return String.fromCodePoint(...[...countryCode].map(char => 127397 + char.charCodeAt()));
+    // Convert Taiwan's country code to China's (if it's TW)
+    if (countryCode.toUpperCase() == 'TW') {
+        countryCode = 'CN'
+    }
+    // Map the Unicode code points for each character in the country code
+    const codePoints = countryCode
+        .toUpperCase()
+        .split('')
+        .map(char => 127397 + char.charCodeAt())
+    // Convert the code points into a string of flag emoji characters
+    return String.fromCodePoint(...codePoints)
 }
