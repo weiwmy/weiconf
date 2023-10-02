055c41b2fa18eed8cde01cdd2c1e228f8033c9ef
weiwmy <weiwmy@gmail.com>
2023-10-02 19:59:36 +0800

    Add all.sh

--- linux/bbr.sh
+++
@@ -1,5 +0,0 @@
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-sysctl net.ipv4.tcp_available_congestion_control
-lsmod | grep bbr
